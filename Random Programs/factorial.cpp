9d46930ca58347e62e9d62ae7b8f31c25dd3e753
Deepak-3012 <deepak.20021230@gmail.com>
2024-07-10 06:24:57 +0530

    random folder added

---
+++ Random Programs/factorial.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findFact(int n){
+	//base case
+	if(n == 0) return 1;
+	// belif
+	int nm1 = findFact(n-1);
+	//expectaion
+	int res = n * nm1;
+	return res;
+
+}
+int main(){
+	int n;
+	cin >> n;
+	int res = findFact(n);
+	cout << res;
+}
